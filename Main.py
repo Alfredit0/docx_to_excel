22c653cdff965bebfdfe650dc735be8d6610b5f5
Alfredo <benitoalfredoreyes@gmail.com>
2018-08-17 14:12:47 -0500

    Testing

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ import WriteExcel
 import io
 import datetime
 now = datetime.datetime.now()
-filePath='MEXICO/AGUASCALIENTES\AGUASCALIENTES.docx'
+filePath='MEXICO/CAMPECHE/CAMPECHE.docx'
 content= io.StringIO(ReadWord.get_docx_text(filePath))
 university = ''
 name = ''
@@ -36,4 +36,4 @@ for line in content.readlines(): #Reading line by line of the content
         mail = ''
         data = ''                
 hrId= now.strftime("%Y-%m-%d_%H_%M")
-WriteExcel.saveDataToExcel(contactsData, 'Contacts-'+hrId)
+WriteExcel.saveDataToExcel(contactsData, 'Contacts-CAMPECHE-'+hrId)
